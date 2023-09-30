47e8ee2c0be4d747eace373e7a5a46151e331dc7
Teuta Krasniqi <tk48167@ubt-uni.net>
2023-09-30 22:28:28 +0200

    Added redux-persist to persist state

--- client/src/redux/rockets/store.js
+++ client/src/redux/rockets/store.js
@@ -1,14 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import rocketRedux from "./rocketRedux";
 import dragonSlice from "./dragonSlice";
 import missionsSlice from "./missionsSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  rockets: rocketRedux,
+  dragons: dragonSlice,
+  missions: missionsSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {
-    rockets: rocketRedux,
-    dragons: dragonSlice,
-    missions: missionsSlice,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;

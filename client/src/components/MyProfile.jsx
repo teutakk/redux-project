6710fe52dc6519df7338dd844e3b84fb34f11e5e
Shnedrit Cocaj <shendritcocaj@hotmail.com>
2023-10-01 16:02:59 +0200

    y

--- client/src/components/MyProfile.jsx
+++ client/src/components/MyProfile.jsx
@@ -26,113 +26,134 @@ const MyProfile = () => {
             </button>
             {showMore ? (
               <div className="cards">
-                {" "}
-                {reservedRockets.map((rocket) => {
-                  return (
-                    <div key={rocket.id}>
-                      <div className="booked-item">
-                        <p className="item-id">
-                          <strong>Rocket id:</strong> {rocket.id}{" "}
-                        </p>
-                        <p className="item-name">
-                          {" "}
-                          <strong>Rocket name:</strong> {rocket.rocket_name}
-                        </p>
-                        <p className="item-description">
-                          {" "}
-                          {rocket.description}
-                        </p>
-                      </div>
-                    </div>
-                  );
-                })}
+                {reservedRockets.length === 0 ? (
+                  <p className="no-item">No Rocket added</p>
+                ) : (
+                  <div>
+                    {" "}
+                    {reservedRockets.map((rocket) => {
+                      return (
+                        <div key={rocket.id}>
+                          <div className="booked-item">
+                            <p className="item-id">
+                              <strong>Rocket id:</strong> {rocket.id}{" "}
+                            </p>
+                            <p className="item-name">
+                              {" "}
+                              <strong>Rocket name:</strong> {rocket.rocket_name}
+                            </p>
+                            <p className="item-description">
+                              {" "}
+                              {rocket.description}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}{" "}
               </div>
             ) : (
-              <p className="no-item">
+              <div className="no-item">
                 {reservedRockets.length === 0 ? (
-                  <p>No item added</p>
+                  <p>No Rocket added</p>
                 ) : (
                   `Rockets added (${reservedRockets.length}) `
                 )}
-              </p>
+              </div>
             )}
           </div>
           <div className="booked">
-            <div className="booked">
-              <p className="booked-title">My Missions</p>
-              <button
-                onClick={() => setShowMission(!showMission)}
-                className="show"
-              >
-                {showMission ? "Hide Missions" : "Show Missions"}
-              </button>
-              {showMission ? (
-                <div>
-                  {reservedMissions.map((mission) => {
-                    return (
-                      <div key={mission.mission_id}>
-                        <div className="booked-item">
-                          <p className="item-id">
-                            {" "}
-                            <strong> Mission id: </strong> {mission.mission_id}{" "}
-                          </p>
-                          <p className="item-name">
-                            <strong> Mission name :</strong>{" "}
-                            {mission.mission_name}
-                          </p>
-                          <p className="item-description">
-                            {" "}
-                            {mission.description}
-                          </p>
+            <p className="booked-title">My Missions</p>
+            <button
+              onClick={() => setShowMission(!showMission)}
+              className="show"
+            >
+              {showMission ? "Hide Missions" : "Show Missions"}
+            </button>
+            {showMission ? (
+              <div className="cards">
+                {reservedMissions.length === 0 ? (
+                  <p className="no-item">No Mission added</p>
+                ) : (
+                  <div>
+                    {" "}
+                    {reservedMissions.map((mission) => {
+                      return (
+                        <div key={mission.id}>
+                          <div className="booked-item">
+                            <p className="item-id">
+                              <strong>Mission id:</strong> {mission.mission_id}
+                            </p>
+                            <p className="item-name">
+                              {" "}
+                              <strong>Mission name:</strong>{" "}
+                              {mission.mission_name}
+                            </p>
+                            <p className="item-description">
+                              {" "}
+                              {mission.description}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              ) : (
-                <p className="no-item">
-                  {reservedMissions.length === 0 ? (
-                    <p>No item added</p>
-                  ) : (
-                    `Missions added (${reservedMissions.length}) `
-                  )}
-                </p>
-              )}
-            </div>
+                      );
+                    })}
+                  </div>
+                )}{" "}
+              </div>
+            ) : (
+              <div className="no-item">
+                {reservedMissions.length === 0 ? (
+                  <p>No Mission added</p>
+                ) : (
+                  `Missions added (${reservedMissions.length}) `
+                )}
+              </div>
+            )}
           </div>
 
           <div className="booked">
             <p className="booked-title">My Dragons</p>
             <button onClick={() => setShowDragon(!showDragon)} className="show">
-              {showDragon ? "Hide Dragons" : "Show Dragons"}
+              {showDragon ? "Hide Rockets" : "Show Rockets"}
             </button>
             {showDragon ? (
-              <div>
-                {reservedDragons.map((dragon) => (
-                  <div key={dragon.id}>
-                    <div className="booked-item">
-                      <p className="item-id">
-                        {" "}
-                        <strong>Dragon ID:</strong> {dragon.id}
-                      </p>
-                      <p className="item-name">
-                        <strong>Dragon Name:</strong> {dragon.name}
-                      </p>
-                      <p className="item-description">
-                        <strong>Dragon Type:</strong> {dragon.type}
-                      </p>
-                    </div>
+              <div className="cards">
+                {reservedDragons.length === 0 ? (
+                  <p className="no-item">No Dragon added</p>
+                ) : (
+                  <div>
+                    {" "}
+                    {reservedDragons.map((dragon) => {
+                      return (
+                        <div key={dragon.id}>
+                          <div className="booked-item">
+                            <p className="item-id">
+                              <strong>Dragon id:</strong> {dragon.id}
+                            </p>
+                            <p className="item-name">
+                              {" "}
+                              <strong>Dragon name:</strong> {dragon.name}
+                            </p>
+                            <p className="item-description">
+                              {" "}
+                              {dragon.description}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
-                ))}
+                )}{" "}
               </div>
             ) : (
-              <p className="no-item">
+              <div className="no-item">
                 {reservedDragons.length === 0 ? (
-                  <p>No item added</p>
+                  <p>No Dragon added</p>
                 ) : (
                   `Dragons added (${reservedDragons.length}) `
                 )}
-              </p>
+              </div>
             )}
           </div>
         </div>
